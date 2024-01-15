d2a5e75b950c533449f16570237c11046aa63d9b
luis-zaldivar <lalozaldivarbarza@gmail.com>
2024-01-15 17:22:14 -0600

    de segundos a milisegundos

--- enf.py
+++ enf.py
@@ -1,9 +1,13 @@
 import re
 from datetime import datetime, timedelta
 
+
 def read_lines(file_path):
-    with open(file_path, 'r') as file:
-        return file.readlines()
+    try:
+        with open(file_path, 'r') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return []
 
 def write_lines(file_path, lines):
     with open(file_path, 'w') as file:
@@ -42,18 +46,26 @@ def separar_por_formato(archivo):
     return lista_final
 
 
-def buscar_en_tupla(lista_tuplas, hora_inicio):
+def buscar_en_tupla(lista_tuplas, hora_inicio, milisegundos_futuros):
     hora_inicio = hora_inicio + ".000"
     formato_hora = '%d-%m-%y;%H:%M:%S.%f'
     hora_inicio_dt = datetime.strptime(hora_inicio, formato_hora)
+    print("Hora de inicio ajustada:", hora_inicio_dt)
+    
     tupla_mas_cercana = min(lista_tuplas, key=lambda tupla: abs(datetime.strptime(tupla[0], formato_hora) - hora_inicio_dt))
-    hora_inicio_nueva = datetime.strptime(tupla_mas_cercana[0], formato_hora) + timedelta(seconds=1)
-    resultados = [tupla for tupla in lista_tuplas if hora_inicio_nueva <= datetime.strptime(tupla[0], formato_hora) <= hora_inicio_nueva + timedelta(minutes=1)]
+    print("Hora más cercana en la lista:", tupla_mas_cercana[0])
+
+    hora_inicio_nueva = datetime.strptime(tupla_mas_cercana[0], formato_hora) + timedelta(milliseconds=milisegundos_futuros)
+    print("Hora de inicio después de ajuste:", hora_inicio_nueva)
+
+    resultados = [tupla for tupla in lista_tuplas if hora_inicio_nueva <= datetime.strptime(tupla[0], formato_hora) <= hora_inicio_nueva + timedelta(milliseconds=1)]
 
     if not resultados:
         print(f"No se encontraron datos desde un segundo después de la hora más cercana: {hora_inicio_nueva.strftime(formato_hora)}")
 
     return resultados
+
+
 
 def depuracion(find_time):
     if not find_time:
@@ -70,10 +82,11 @@ def depuracion(find_time):
     return find_time
 
 if __name__ == "__main__":
-    archivo_entrada = "ems.cap"
-    archivo_salida = "ems_limpio.txt"
+    archivo_entrada = "EMS CLASICO.txt"
+    archivo_salida = "ems_limpio1.txt"
     depurar_archivo(archivo_entrada, archivo_salida)
     eliminar_texto_previo(archivo_entrada, archivo_salida)
-    salida = separar_por_formato('ems_limpio.txt')
-    find_time = buscar_en_tupla(salida, '24-01-11;14:13:59')
+    salida = separar_por_formato('ems_limpio1.txt')
+    find_time = buscar_en_tupla(salida, '24-01-04;09:15:11', 1000)  # Ampliar el rango a 1000 milisegundos (1 segundo)
+                                                                    # '24-01-11;14:13:59'
     find_time = depuracion(find_time)
